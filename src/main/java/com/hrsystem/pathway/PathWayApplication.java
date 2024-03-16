34585c05b2a94a2d6d2b69884b0f98b92ad503b3
Vasilije Čabarkapa <vascabarkapa@gmail.com>
2024-03-16 21:00:19 +0100

    spring initializer

---
+++ src/main/java/com/hrsystem/pathway/PathWayApplication.java
@@ -0,0 +1,13 @@
+package com.hrsystem.pathway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PathWayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PathWayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hrsystem/pathway/PathWayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrsystem.pathway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PathWayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
