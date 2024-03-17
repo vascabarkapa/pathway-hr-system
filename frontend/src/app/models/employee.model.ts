38bda576c90dd920b41c9163d880e91e5b848e90
Vasilije Čabarkapa <vascabarkapa@gmail.com>
2024-03-17 21:59:00 +0100

    employee front model

---
+++ frontend/src/app/models/employee.model.ts
@@ -0,0 +1,10 @@
+export interface Employee {
+    id: number;
+    firstName: string;
+    lastName: string;
+    email: string;
+    jobTitle: string;
+    phoneNumber: string;
+    imageUrl: string;
+    code: string;
+}
